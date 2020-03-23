65a530d4a095e10b49784e09139a04666be9b6f2
quachhuytung <huytung.9729@gmail.com>
2020-03-23 13:35:07 +0700

    [FIX] Pricing format for car detail

--- bonbanh/utils.py
+++ bonbanh/utils.py
@@ -8,7 +8,11 @@ def get_date(_, text):
 def get_price_vnd(_, text):
     price_vnd_pattern = re.compile(r"\d+")
     price_vnd_unformated = text[0].split("-")[-1]
-    return "".join(price_vnd_pattern.findall(price_vnd_unformated))
+    raw_data = price_vnd_pattern.findall(price_vnd_unformated)
+    if(len(raw_data)==1):
+        return raw_data[0]
+    else:
+        return str(int(raw_data[0])*1000+int(raw_data[1]))
 
 def get_price_dol(_, text):
     return text[0][3:-2]
